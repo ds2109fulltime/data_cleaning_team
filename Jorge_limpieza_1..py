e6f8635b65cf5645281aa0457fcd1134b6796a36
JorgeDuqueP <jorgeduke76@gmail.com>
2021-12-14 16:58:53 +0100

    Funciones modificadas

--- Jorge_limpieza_1..py
+++ Jorge_limpieza_1..py
@@ -1,4 +1,6 @@
-def numeric(columns, df):
+new_df = df[['name', 'sex', 'ticket', 'embarked', 'boat', 'home.dest']]
+
+def numeric(columns):
     '''Esta función es para cambiar los valores categóricos a valores numéricos de una columna concreta
     Los parámetros son:
     columns = columna a modificar
@@ -9,4 +11,4 @@ def numeric(columns, df):
     for i in columns:
         le.fit(df[i])
         df[i] = le.transform(df[i])
-    return columns
+numeric(columns=new_df)
--- Jorge_limpieza_4..py
+++ Jorge_limpieza_4..py
@@ -1,9 +1,12 @@
-def drop_NaN(df, df_column):
+df_str = df[['pclass', 'survived', 'sibsp', 'parch', 'age', 'fare', 'body']]
+def drop_NaN(df, column):
     '''This function eliminates the NaN values of a specific column
     Los parámetros que necesitamos son:
     df = DataFrame
     df_column = columna de la que queremos eliminar los valorers NaN
     '''
     import numpy as np
-    df_column.replace(0,np.nan)
-    df.dropna(axis=0, implace=True)
+    import pandas as pd
+    column.replace(0,np.nan)
+    df.dropna(axis=0, inplace=True)
+drop_NaN(df=df, column=df['body'])
