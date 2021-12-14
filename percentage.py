be60e6a13db53ba30959242420af690c860eae78
martagomezz <91martagomez@gmail.com>
2021-12-14 20:56:43 +0100

    ’update’

--- percentage.py
+++ percentage.py
@@ -1,13 +1,13 @@
-def percentage(data,column):
+def percentage(df,column):
         
     '''
-    This functions become your column with % to decimal column of your data.
+    This functions become your column with % to decimal column of your dataframe.
     
-    data: dataframe
+    df: dataframe
     column: column to be represented.
     '''
 
     import pandas as pd
-    x = data[column].str.replace('%', '').astype(float)
-    data[column] = x/100
-    return data[column]
+    x = df[column].str.replace('%', '').astype(float)
+    df[column] = x/100
+    return df[column]
