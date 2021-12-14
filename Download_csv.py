3c2de79c2d12000b1a6a480a9f2e8c5558ca971b
Pierots08 <90680555+Pierots08@users.noreply.github.com>
2021-12-14 14:22:40 +0100

    fix functions

--- Download_csv.py
+++ Download_csv.py
@@ -1,6 +1,8 @@
+import requests
+
 def download_csv(url):
     '''This Functions takes a URL with a CSV file and downloads it in the same folder the function is being run.
     The only required parameter is the URL itself'''
-    import requests
+    
     req = requests.get(url)
     url_content = req.content
